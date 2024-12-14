5c547ddc9ef9d2f0eb7333db532577aa9b507152
aman991932 <aman.991932@gmail.com>
2024-12-14 09:41:35 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,20 @@
+class script(object):
+    START_TXT = """<b>ʜᴇʏ {} 👋, \nɪ ᴀᴍ <a href='https://t.me/AV_IMA_TO_URL_BOT'>ɪᴍᴀɢᴇ ᴛᴏ ʟɪɴᴋ</a> ʙᴏᴛ ɪᴜꜱᴛ ꜱᴇɴᴅ ᴛᴏ ʏᴏᴜʀ ɪᴍᴀɢᴇ & ᴠɪᴅᴇᴏ ᴀɴᴅ 𝟻 ᴍʙ.\n\nMʏ Cʀᴇᴀᴛᴏʀ : <a href='https://t.me/AV_MOVIE_HOUSE'>ᴀᴠ ʙᴏᴛᴢ</a></b>"""
+    
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Nᴀᴍᴇ - {}"""
+    
+    
+
+    AMAN_TXT = """<b>╔════❰ <a href='https://t.me/AV_IMA_TO_URL_BOT'>ɪᴍᴀɢᴇ ᴛᴏ ʟɪɴᴋ</a> ❱═❍
+║╭━━━━━━━━━━━━━━━➣
+║┣⪼🤖ᴍʏ ɴᴀᴍᴇ : <a href='https://t.me/AV_IMA_TO_URL_BOT'>ɪᴍᴀɢᴇ ᴛᴏ ʟɪɴᴋ</a>
+║┣⪼👦ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/AV_OWNER_BOT'>ᴀᴠ ᴄʜᴀᴛ ᴏᴡɴᴇʀ</a>
+║┣⪼❣️ᴜᴘᴅᴀᴛᴇ : <a href=https://t.me/AV_BOTz_UPDATE>ᴀᴠ ʙᴏᴛᴢ</a>
+║┣⪼🗣️ʟᴀɴɢᴜᴀɢᴇ : <a href=https://www.python.org>ᴘʏᴛʜᴏɴ 3</a>
+║┣⪼📚ʟɪʙʀᴀʀʏ : <a href=https://github.com/pyrogram>ᴘʏʀᴏɢʀᴀᴍ</a> 
+║┣⪼🗒️ᴠᴇʀsɪᴏɴ : v2.01.1
+║╰━━━━━━━━━━━━━━━➣
+╚══════════════════❍ </b>"""
+
